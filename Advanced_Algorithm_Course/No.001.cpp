d98bc80f83db0ccfe8a998ffeaf9d38692d47f92
Marathon <3045130265@qq.com>
2026-03-08 21:44:18 +0800

    283-移动零

---
+++ Advanced_Algorithm_Course/No.001.cpp
@@ -0,0 +1,16 @@
+//  https://leetcode.cn/problems/move-zeroes/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int j = 0;
+        for(int i =0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+    }
+};
