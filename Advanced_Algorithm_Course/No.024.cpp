b88fe4c382b62ef5d3e7a7b15cc3f00f0239a74f
csa <3045130265@qq.com>
2026-04-19 21:53:40 +0800

    LCR 173-点名

---
+++ Advanced_Algorithm_Course/No.024.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.cn/problems/que-shi-de-shu-zi-lcof/
+
+class Solution
+{
+public:
+    int takeAttendance(vector<int>& records)
+    {
+        int left = 0;
+        int right = records.size() - 1;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (records[mid] == mid) left = mid + 1;
+            else right = mid;
+        }
+        return records[left] == left ? left + 1 : left;
+        //return left + 1; 
+    }
+};
