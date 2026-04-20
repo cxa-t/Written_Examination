40a4c94b133be8c33f305d02b223d036382b5b28
csa <3045130265@qq.com>
2026-04-20 16:58:59 +0800

    724-寻找数组的中心下标

---
+++ Advanced_Algorithm_Course/No.027.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.cn/problems/find-pivot-index/
+
+class Solution
+{
+public:
+    int pivotIndex(vector<int>& nums)
+    {
+        vector<int> leftSum(nums.size(), 0);
+        vector<int> rightSum(nums.size(), 0);
+        for (int i = 1; i < nums.size(); i++)
+            leftSum[i] = leftSum[i - 1] + nums[i - 1];
+        for (int i = nums.size() - 2; i >= 0; i--)
+            rightSum[i] = rightSum[i + 1] + nums[i + 1];
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (leftSum[i] == rightSum[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
