1c72e37f85e3d6f2c89dab95ec72233b1ff29732
csa <3045130265@qq.com>
2026-04-25 17:04:04 +0800

    双指针-2540-最小公共值

---
+++ LeetCodeOther/2540.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.cn/problems/minimum-common-value/
+
+class Solution
+{
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2)
+    {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int i1 = 0;
+        int i2 = 0;
+        while (i1 < n1 && i2 < n2)
+        {
+            if (nums1[i1] == nums2[i2]) return nums1[i1];
+            else if (nums1[i1] > nums2[i2]) i2++;
+            else i1++;
+        }
+        return -1;
+    }
+};
