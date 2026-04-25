cb6e01a96977f01ae9d67d3ee5b08f832d9b2f99
csa <3045130265@qq.com>
2026-04-25 16:23:59 +0800

    排除法-240-搜索二维矩阵II

---
+++ LeetCodeOther/240.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.cn/problems/search-a-2d-matrix-ii/
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = 0;
+        int j = n - 1;
+        while (i < m && j >= 0)
+        {
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] > target) j--;
+            else i++;
+        }
+        return false;
+    }
+};
