30887a7d181146a1bead99df0b5c1443fc522bab
csa <3045130265@qq.com>
2026-04-26 15:54:30 +0800

    双指针-344-反转字符串

---
+++ LeetCodeOther/344.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.cn/problems/reverse-string/
+
+class Solution
+{
+public:
+    void reverseString(vector<char>& s)
+    {
+        int left = 0;
+        int right = s.size() - 1;
+        while (left < right)
+        {
+            std::swap(s[left++], s[right--]);
+        }
+    }
+};
