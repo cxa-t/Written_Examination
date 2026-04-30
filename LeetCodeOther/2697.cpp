c256e833b006ffd0016b2da7150c4af3dd30664a
csa <3045130265@qq.com>
2026-04-30 19:42:32 +0800

    双指针-2697-字典序最小回文串

---
+++ LeetCodeOther/2697.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.cn/problems/lexicographically-smallest-palindrome/
+
+class Solution
+{
+public:
+    string makeSmallestPalindrome(string s)
+    {
+        int left = 0;
+        int right = s.size() - 1;
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                if (s[left] < s[right]) s[right] = s[left];
+                else s[left] = s[right];
+            }
+            left++;
+            right--;
+        }
+        return s;
+    }
+};
