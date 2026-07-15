4faec6b11a2339fa84c07ff5e984cc8c988a73a5
cxa-t <134744746+cxa-t@users.noreply.github.com>
2026-07-15 20:40:12 +0800

    双数组dp-44-通配符匹配

---
+++ Advanced_Algorithm_Course/44.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.cn/problems/wildcard-matching/
+
+class Solution
+{
+public:
+    // 判断字符串 s 是否能被模式串 p 完全匹配
+    bool isMatch(string s, string p)
+    {
+        // m 表示字符串 s 的长度，n 表示模式串 p 的长度
+        int m = s.size(), n = p.size();
+        // 在 s 和 p 前面加一个空格，使下标从 1 开始，方便 dp 对应
+        s = " " + s, p = " " + p;
+        // dp[i][j] 表示 s[1...i] 是否能被 p[1...j] 匹配
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+        // 空字符串和空模式串可以匹配
+        dp[0][0] = true;
+        // 初始化 s 为空时，p 的前缀是否可以匹配空字符串
+        for (int j = 1; j <= n; j++)
+        {
+            // 如果 p[j] 是 '*'，它可以匹配空字符串
+            if (p[j] == '*')
+                dp[0][j] = true;
+            // 一旦遇到非 '*' 字符，就无法继续匹配空字符串
+            else
+                break;
+        }
+        // 枚举字符串 s 的前 i 个字符
+        for (int i = 1; i <= m; i++)
+        {
+            // 枚举模式串 p 的前 j 个字符
+            for (int j = 1; j <= n; j++)
+            {
+                // 如果当前模式字符是 '*'，它可以匹配空串或匹配一个及以上字符
+                if (p[j] == '*')
+                    // dp[i][j-1] 表示 '*' 匹配空串，dp[i-1][j] 表示 '*' 匹配当前 s[i]
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                // 如果当前模式字符不是 '*'
+                else
+                    // 当前字符能匹配，并且前面的部分也能匹配，当前状态才为 true
+                    dp[i][j] = (p[j] == '?' || s[i] == p[j]) && dp[i - 1][j - 1];
+            }
+        }
+        // 返回整个字符串 s 是否能被整个模式串 p 匹配
+        return dp[m][n];
+    }
+};
