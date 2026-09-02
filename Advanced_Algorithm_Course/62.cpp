5e8f4fd15080bbb1fea5316ee00471d98c336bd7
cxa-t <134744746+cxa-t@users.noreply.github.com>
2026-09-02 18:24:15 +0800

    记忆化搜索-62-不同路径

--- Advanced_Algorithm_Course/62.cpp
+++ Advanced_Algorithm_Course/62.cpp
@@ -27,3 +27,34 @@ public:
         return dp[m][n];
     }
 };
+
+
+class Solution
+{
+public:
+    int uniquePaths(int m, int n)                         // 计算从左上角到右下角的不同路径数量
+    {
+        vector<vector<int>> memo(m + 1, vector<int>(n + 1, -1)); // 初始化记忆数组保存子问题结果
+        dfs(memo, m, n);                                  // 从终点开始递归计算路径数量
+        return memo[m][n];                                // 返回最终路径数量
+    }
+    int dfs(vector<vector<int>>& memo, int i, int j)      // 计算到达(i,j)位置的路径数量
+    {
+        if (i == 0 || j == 0)                              // 判断是否越过网格边界
+        {
+            memo[i][j] = 0;                               // 边界外位置没有有效路径
+            return 0;                                    // 返回无效路径数量
+        }
+        if (i == 1 && j == 1)                              // 判断是否到达起点位置
+        {
+            memo[i][j] = 1;                               // 起点到自身只有一种路径
+            return 1;                                    // 返回起点路径数量
+        }
+        if (memo[i][j] != -1)                              // 判断当前子问题是否已经计算
+        {
+            return memo[i][j];                            // 直接返回缓存结果
+        }
+        memo[i][j] = dfs(memo, i - 1, j) + dfs(memo, i, j - 1); // 当前路径数等于上方和左方路径数之和
+        return memo[i][j];                                // 返回当前位置的路径数量
+    }
+};
